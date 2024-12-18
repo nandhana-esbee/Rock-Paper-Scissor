09540fac807e96c17bda543e12c45f7534d74330
Nandhana SB <nandhanaesbee16@gmail.com>
2024-12-18 18:42:59 +0530

    Client added

---
+++ Client/rock-paper-scissor/src/components/App.js
@@ -0,0 +1,9 @@
+import Heading from "./Head";
+
+function App() {
+  return (
+   <div><Heading /></div>
+  );
+}
+
+export default App;
---
+++ Client/rock-paper-scissor/src/components/Head.js
@@ -0,0 +1,6 @@
+
+function Heading(){
+    return <h1>Hello</h1>;
+}
+
+export default Heading;
